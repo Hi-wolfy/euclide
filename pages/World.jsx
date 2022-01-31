8f16d76920920a958932a328d1e395ba166d7d18
hi-wolfy <hi-wolfy@hi-wolfyui-Macmini.local>
2022-01-31 16:19:42 +0900

    slide수정

--- pages/World.jsx
+++ pages/World.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import Slider from '../components/Slider';
-import MobileSlider from '../components/MobileSlider';
+
 export default function World() {
   return (
     <div className="worldWrap">
       <img className="worldTitle" src="img/world.png" width="180px" />
-      <p className="mobileNone">
-        {' '}
-        <Slider />{' '}
-      </p>
-      <p className="PCNone">
-        {' '}
-        <MobileSlider />
-      </p>
+      <Slider />
 
       {/* <div className="worldRow">
         <img onClick={SwalPlay} src="img/letter-cont2.png" />
