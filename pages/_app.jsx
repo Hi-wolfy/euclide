c4d4664ec373f5c26298f05d69256e7061880f4a
ireh1214 <hireh1214@gmail.com>
2022-02-04 16:44:37 +0900

    bot옮겨옴

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -7,6 +7,68 @@ import '../styles/guest.scss';
 import '../styles/index.scss';
 import Header from '../components/Header';
 import Script from 'next/script';
+// import ChatBot from 'react-simple-chatbot';
+
+
+
+// const steps=[
+//       {
+//         id: '1',
+//         message: '안녕하세요? 아치가 지금 무슨 생각을 하고있을까요?',
+//         trigger: '2',
+//       },
+//       {
+//         id: '2',
+//         options: [
+//           { value: 1, label: '아 퇴근하고싶다...', trigger: '4' },
+//           { value: 2, label: '아 배고프다', trigger: '3' },
+//           { value: 3, label: '커뮤 빨리 개장했으면', trigger: '3' },
+//         ],
+//       },
+//       {
+//         id: '3',
+//         message: '아닌데요?',
+//         trigger: '2',
+//       },
+//       {
+//         id: '4',
+//         message: '맞았습니다!',
+//            trigger: '5',
+//       },
+//         {
+//         id: '5',
+//         message: '그러면 제가 지금 생각하는 숫자는요?',
+//            trigger: '6',
+//       },
+//         {
+//         id: '6',
+//       options: [
+//           { value: 1, label: '3이요!', trigger: '7' },
+//           { value: 2, label: '6이요!', trigger: '8' },
+//           { value: 3, label: '9요!', trigger: '7' },
+//         ],
+//       },
+//          {
+//         id: '7',
+//         message: '틀렸는데요!?',
+//           trigger: '6',
+//       },
+//          {
+//         id: '8',
+//         message: '맞았습니다!',
+//               trigger: '9',
+//       },
+//          {
+//         id: '9',
+//         message: '흠 이제 슬슬 끝내죠!',
+//             trigger: '10',
+//       },
+//        {
+//         id: '10',
+//         message: '안녕히계세요!',
+//         end: true,
+//       },
+//     ]
 
 function MyApp({ Component, pageProps }) {
 
@@ -42,7 +104,8 @@ function MyApp({ Component, pageProps }) {
      
       <Header />
       <Component {...pageProps} />
-     
+ {/* <ChatBot steps={steps} />; */}
+
     </div>
   );
 }
